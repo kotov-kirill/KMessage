883a821c49d213dd2d89b8416fcb1b61134e09e1
kotov-kirill <kirsoft95@mail.ru>
2016-02-05 11:35:06 +0300

    Add FloatingActionButton

--- app/src/main/java/com/example/kirill/kmessage/MessagesActivity/MessagesActivity.java
+++ app/src/main/java/com/example/kirill/kmessage/MessagesActivity/MessagesActivity.java
@@ -3,13 +3,16 @@ package com.example.kirill.kmessage.MessagesActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ListView;
 
@@ -39,6 +42,7 @@ public class MessagesActivity extends AppCompatActivity {
     private void initComponents() {
         this.initToolBar();
         this.initNavigationView();
+        this.initFloatingActionButton();
         this.initListView();
     }
 
@@ -61,6 +65,17 @@ public class MessagesActivity extends AppCompatActivity {
                 drawerLayout.closeDrawers();
                 MenuReceiver.menuReceiver(MessagesActivity.this, item);
                 return true;
+            }
+        });
+    }
+
+    private void initFloatingActionButton() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
